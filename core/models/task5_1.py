c06a41b6d84db64c624bbb32fe6edb88ba0ea34f
Arseniy Zelenichenko <117036995+ze2ar@users.noreply.github.com>
2025-03-25 23:45:52 +0500

    feat: Change input parameters for service_task5_1

--- core/models/task5_1.py
+++ core/models/task5_1.py
@@ -6,10 +6,15 @@ class PoissonRequest(BaseModel):
     Модель для расчета вероятности по формуле Пуассона.
     """
 
-    a: float = Field(
-        description="Среднее число событий за интервал времени (λ)", examples=[2.57]
+    n: int = Field(
+        ge=0,
+        description="Общее количество испытаний",
+        examples=[900],
     )
-    m: int = Field(ge=0, description="Число наступлений события (m)", examples=[2])
+    p: float = Field(
+        ge=0, le=1, description="Вероятность успеха в одном испытании", examples=[0.005]
+    )
+    m: int = Field(ge=0, description="Число наступлений события (m)", examples=[3])
 
 
 class LocalMoivreLaplaceRequest(BaseModel):
@@ -17,10 +22,13 @@ class LocalMoivreLaplaceRequest(BaseModel):
     Модель для расчета вероятности по локальной теореме Муавра-Лапласа.
     """
 
-    sigma: float = Field(
-        description="Среднеквадратическое отклонение (σ)", examples=[2.57]
+    n: int = Field(ge=0, description="Общее количество испытаний", examples=[900])
+    p: float = Field(
+        ge=0, le=1, description="Вероятность успеха в одном испытании", examples=[0.005]
     )
-    x_m: float = Field(description="Стандартизированное значение x_m", examples=[-3.48])
+    x_target: int = Field(
+        ge=0, description="Целевое количество успехов (x)", examples=[3]
+    )
 
 
 class IntegralMoivreLaplaceRequest(BaseModel):
@@ -28,9 +36,11 @@ class IntegralMoivreLaplaceRequest(BaseModel):
     Модель для расчета вероятности по интегральной теореме Муавра-Лапласа.
     """
 
-    a: float = Field(
-        description="Левая граница нормированного значения", examples=[-3.48]
+    n: int = Field(ge=0, description="Общее количество испытаний", examples=[400])
+    p: float = Field(
+        ge=0, le=1, description="Вероятность успеха в одном испытании", examples=[0.1]
     )
-    b: float = Field(
-        description="Правая граница нормированного значения", examples=[3.48]
+    x1: int = Field(ge=0, description="Левая граница количества успехов", examples=[30])
+    x2: int = Field(
+        ge=0, description="Правая граница количества успехов", examples=[50]
     )
